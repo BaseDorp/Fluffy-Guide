163b6e9be332720293592b90f129fdfc983caed8
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2020-12-11 13:47:55 -0600

    In class Progress

--- Assets/Scripts/Cat_Ai.cs
+++ Assets/Scripts/Cat_Ai.cs
@@ -11,6 +11,8 @@ public class Cat_Ai : MonoBehaviour
     
     [SerializeField]
     float speed;
+    [SerializeField]
+    float fleeDistance;
     
     // Start is called before the first frame update
     void Start()
@@ -21,35 +23,38 @@ public class Cat_Ai : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //RaycastHit hit;
-        //if (Physics.Raycast(ray, out hit, Mathf.Infinity))
-        //{
-        //    // Place of mouse
-        //    Debug.Log(hit.point);
-        //}
 
-        Flee();
+       // Flee();
 
     }
 
     void Flee()
     {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 0;
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+
+
         
-
-
-        //// should be the opposite direction of the mouse
-        //Vector3 targetPos = this.transform.position - mousePos;
-        //targetPos.z = 0;
-        //transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        if (Vector3.Distance(this.transform.position, mousePos) <= fleeDistance)
+        {
+            // should be the opposite direction of the mouse
+            Vector3 targetPos = this.transform.position - mousePos;
+            targetPos.z = 0;
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("asfa");
+        Debug.Log("collided");
         if (collision.name == "Hand" && caught == false)
         {
-            Debug.Log("cojkaos");
+            Debug.Log("running");
             Vector3 mousePos = Input.mousePosition;
             mousePos.z = 0;
             Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
@@ -64,6 +69,7 @@ public class Cat_Ai : MonoBehaviour
 
         if (collision.tag == "Cage")
         {
+            Debug.Log("caught");
             caught = true;
         }
     }
