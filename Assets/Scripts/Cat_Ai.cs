4335e51a95bc81ae6ea06f79e572fa03aa9a2c5d
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2020-12-18 02:06:30 -0600

    adds level complete screen when cats are collected and bounds for cats to spawn in

--- Assets/Scripts/Cat_Ai.cs
+++ Assets/Scripts/Cat_Ai.cs
@@ -6,8 +6,10 @@ public class Cat_Ai : MonoBehaviour
 {
 
     SpriteRenderer catSprite;
+    CircleCollider2D circleCollider;
 
     bool caught = false;
+    float lastFramePos = 0;
     
     [SerializeField]
     float speed;
@@ -21,7 +23,9 @@ public class Cat_Ai : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.catSprite = GetComponent<SpriteRenderer>();
+        this.circleCollider = GetComponent<CircleCollider2D>();
+        circleCollider.enabled = true;
     }
 
     // Update is called once per frame
@@ -32,10 +36,8 @@ public class Cat_Ai : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        Debug.Log("collided");
         if (collision.name == "Hand" && caught == false)
         {
-            Debug.Log("running");
             Vector3 mousePos = Input.mousePosition;
             mousePos.z = 0;
             Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
@@ -45,12 +47,25 @@ public class Cat_Ai : MonoBehaviour
 
             Vector3 targetPos = this.transform.position - mousePos;
             targetPos.z = 0;
+
+            
+            if (lastFramePos < this.transform.position.x)
+            {
+                catSprite.flipX = true;
+            }
+            else if (lastFramePos > this.transform.position.x)
+            {
+                catSprite.flipX = false;
+            }
+
             transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+            lastFramePos = this.transform.position.y;
         }
 
         if (collision.tag == "Cage")
         {
             Debug.Log("caught");
+            circleCollider.enabled = false;
             caught = true;
             OnCatHerded();
         }
